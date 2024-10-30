b2dbbf38ceda2640c206208bafd4cbfe0098b5db
CSEAryan <dipuaryan1111@gmail.com>
2024-10-30 10:30:57 +0545

    folder structure updated

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path  #we can create system compatible path
+
+#creating files and folders
+
+list_of_files =[
+
+
+    ".github/workflows/.gitkeep",
+    "src/__init__.py", #source code file
+    "src/components/__init__.py",  #all the stages
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception"
+    "tests/unit/__init__.py",  #for unit testing
+    "tests/integration/__init__.py", #for integration testing
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb",
+
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath) #splitting to get dir and file seperately
+    if filedir != "": #if not empty wwe have to create directory
+        os.makedirs(filedir, exist_ok= True)
+        #logging.info(f"Creating directory: {filedir} for file: {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass  #creating an empty file
